bac1c1094a5171bd80b3b0a0de83528f4172134b
Anne Flore Bernard <b.anneflore@yahoo.fr>
2025-02-14 15:12:21 +0100

    routes

--- backend/src/infrastructure/web/api/user_routes.py
+++ backend/src/infrastructure/web/api/user_routes.py
@@ -1,5 +1,6 @@
 from werkzeug.utils import secure_filename
 from flask import Blueprint, jsonify, request, current_app
+from src.domain.models.user import User
 
 from src.application.services.user_service import UserService
 
@@ -13,6 +14,63 @@ def create_user_routes(user_service: UserService) -> Blueprint:
             return jsonify({"message": "Liste des utilisateurs récupérée", "content": [vars(user) for user in users], "error": None}), 200
         except Exception as e:
             current_app.logger.error(f"Erreur récupération utilisateurs: {str(e)}")
+            return jsonify({"message": "Erreur serveur", "content": None, "error": str(e)}), 500
+
+    @user_routes.route('/users/<int:id>', methods=['GET'])
+    def get_user(id):
+        try:
+            user = user_service.get_user(id)
+            return jsonify({"message": "User", "content": vars(user), "error": None}), 200
+        except Exception as e:
+            current_app.logger.error(f"Erreur récupération utilisateurs: {str(e)}")
+            return jsonify({"message": "Erreur serveur", "content": None, "error": str(e)}), 500
+        
+    @user_routes.route('/users', methods=['POST'])
+    def create_user():
+        try:
+            data = request.get_json()
+
+            print(data)
+
+            if not all(k in data for k in ["email", "first_name", "last_name", "password"]):
+                return jsonify({"message": "Données manquantes", "content": None, "error": "Champ(s) requis absent(s)"}), 400
+
+            new_user = User(
+                email=data["email"],
+                first_name=data["first_name"],
+                last_name=data["last_name"],
+                password=data["password"],
+                is_active=data.get("is_active", True),
+                user_role=data.get("user_role", "client")
+            )
+
+            saved_user = user_service.create_user(new_user)
+
+            return jsonify({"message": "Utilisateur créé", "content": vars(saved_user), "error": None}), 201
+        except Exception as e:
+            current_app.logger.error(f"Erreur création utilisateur: {str(e)}")
+            return jsonify({"message": "Erreur serveur", "content": None, "error": str(e)}), 500
+
+    @user_routes.route('/users/<int:id>', methods=['PUT'])
+    def update_user(id):
+        try:
+            data = request.get_json()
+            updated_user = user_service.update_user(id, data)
+            return jsonify({"message": "User updated", "content": vars(updated_user), "error": None}), 200
+        except Exception as e:
+            current_app.logger.error(f"Erreur mise à jour utilisateur: {str(e)}")
+            return jsonify({"message": "Erreur serveur", "content": None, "error": str(e)}), 500
+
+    @user_routes.route('/users/<int:id>', methods=['DELETE'])
+    def delete_user(id):
+        try:
+            success = user_service.delete_user(id)
+            if not success:
+                return jsonify({"message": "User not find", "content": None, "error": None}), 404
+            
+            return jsonify({"message": "User deleted", "content": None, "error": None}), 204
+        except Exception as e:
+            current_app.logger.error(f"Erreur suppression utilisateur: {str(e)}")
             return jsonify({"message": "Erreur serveur", "content": None, "error": str(e)}), 500
 
     return user_routes
