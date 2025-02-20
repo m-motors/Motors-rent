d066aa6e5d2efa66450654a7f4f4d37a45d84da7
CharleryM <charlerymalcolm.l@gmail.com>
2025-02-20 18:51:57 +0100

    debug form voiture

--- frontend/src/components/vehicles/AddVehicle.tsx
+++ frontend/src/components/vehicles/AddVehicle.tsx
@@ -37,67 +37,66 @@ const AddVehicle = () => {
         <Header />
         <div className="add_page">
         <div className="add_page_card shadow-2xl max-w-2xl">
-            <h2>Créer une Annonce de Voiture</h2>
+            <h2>une Annonce de Voiture</h2>
             <form onSubmit={handleSubmit}>
             <div className="mb-4">
                 <label className="add_user_label" htmlFor="brand">
                   Marque
                 </label>
-                <input value={vehicleData.brand} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
+                <input name="brand" value={vehicleData.brand} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
             </div>
             <div className="mb-4">
                 <label className="add_user_label" htmlFor="model">
                 Modèle
                 </label>
-                <input value={vehicleData.model} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
+                <input name="model" value={vehicleData.model} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
             </div>
             <div className="mb-4">
                 <label className="add_user_label" htmlFor="year">
                 Année
                 </label>
-                <input value={vehicleData.year} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
+                <input name="year" value={vehicleData.year} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
             </div>
             <div className="mb-4">
                 <label className="add_user_label" htmlFor="horsepower">
                 Puissance (CV)
                 </label>
-                <input value={vehicleData.horsepower} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
+                <input name="horsepower" value={vehicleData.horsepower} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
             </div>
             <div className="mb-4">
                 <label className="add_user_label" htmlFor="price">
                 Prix (€)
                 </label>
-                <input value={vehicleData.price} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
+                <input name="price" value={vehicleData.price} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
             </div>
             <div className="mb-4">
                 <label className="add_user_label" htmlFor="category">
                 Catégorie
                 </label>
-                <input value={vehicleData.category} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
+                <input name="category" value={vehicleData.category} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
             </div>
             <div className="mb-4">
                 <label className="add_user_label" htmlFor="motor">
                 Moteur
                 </label>
-                <input value={vehicleData.motor} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
+                <input name="motor" value={vehicleData.motor} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
             </div>
             <div className="mb-4">
                 <label className="add_user_label" htmlFor="color">
                 Couleur
                 </label>
-                <input value={vehicleData.color} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
+                <input name="color" value={vehicleData.color} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
             </div>
             <div className="mb-4">
                 <label className="add_user_label" htmlFor="mileage">
                 Kilométrage (km)
                 </label>
-                <input value={vehicleData.mileage} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
+                <input name="mileage" value={vehicleData.mileage} onChange={handleChange} className="shadow focus:outline-none focus:shadow-outline" required/>
             </div>
             <div className="div_button">
                 <button
                 type="submit"
-                className="hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                >
+                className="hover:bg-blue-700 focus:outline-none focus:shadow-outline">
                 Créer l'Annonce
                 </button>
             </div>
